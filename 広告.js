099bcdd7d53b4cd25236e35a4d3667755bafd192
yamath <95947812+tiger-math@users.noreply.github.com>
2022-01-20 19:31:56 +0900

    Add files via upload

---
+++ 広告.js
@@ -0,0 +1,23 @@
+const siteName1 = document.getElementById('siteName-1');
+const siteName2 = document.getElementById('siteName-2');
+
+const siteText1 = document.getElementById('siteText-1');
+const siteText2 = document.getElementById('siteText-2');
+
+const siteImg1 = document.getElementById('siteImg-1');
+const siteImg2 = document.getElementById('siteImg-2');
+
+const siteA1 = document.getElementById('siteA-1');
+const siteA2 = document.getElementById('siteA-2');
+
+siteName1.innerText = "";
+siteText1.innerText = "";
+siteImg1.src = "";
+siteA1.href = "";
+siteA1.title = "";
+
+siteName2.innerText = "";
+siteText2.innerText = "";
+siteImg2.src = "";
+siteA2.href = "";
+siteA2.title = "";
