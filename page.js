d4807cfe19c28ec18ece6ce0f9de060f994bc46f
yamath <95947812+tiger-math@users.noreply.github.com>
2022-01-20 21:41:33 +0900

    Update page.js

--- page.js
+++ page.js
@@ -1,23 +1,21 @@
+//広告
 const siteName1 = document.getElementById('siteName-1');
 const siteName2 = document.getElementById('siteName-2');
-
 const siteText1 = document.getElementById('siteText-1');
 const siteText2 = document.getElementById('siteText-2');
-
 const siteImg1 = document.getElementById('siteImg-1');
 const siteImg2 = document.getElementById('siteImg-2');
-
 const siteA1 = document.getElementById('siteA-1');
 const siteA2 = document.getElementById('siteA-2');
 
-siteName1.innerText = "";
-siteText1.innerText = "";
-siteImg1.src = "";
-siteA1.href = "";
-siteA1.title = "";
+siteName1.innerText = ""; //1つ目の広告の見出し
+siteText1.innerText = ""; //1つ目の広告の説明
+siteImg1.src = "";　　　　//1つ目の広告の画像のリンク
+siteA1.href = "";　　　　 //1つ目の広告のサイトのリンク
+siteA1.title = "";　　　　//1つ目の広告のtitle
 
-siteName2.innerText = "";
-siteText2.innerText = "";
-siteImg2.src = "";
-siteA2.href = "";
-siteA2.title = "";
+siteName2.innerText = ""; //2つ目の広告の見出し
+siteText2.innerText = ""; //2つ目の広告の説明
+siteImg2.src = "";　　　　//2つ目の広告の画像のリンク
+siteA2.href = "";　　　　 //2つ目の広告のサイトのリンク
+siteA2.title = "";　　　　//2つ目の広告のtitle
