bb76fb5a3e6c37dbbc15d31b803d4e059e29482d
yamath <95947812+tiger-math@users.noreply.github.com>
2022-01-22 10:41:11 +0900

    Update page.js

--- page.js
+++ page.js
@@ -8,14 +8,14 @@ const siteImg2 = document.getElementById('siteImg-2');
 const siteA1 = document.getElementById('siteA-1');
 const siteA2 = document.getElementById('siteA-2');
 
-siteName1.innerText = ""; //1つ目の広告の見出し
-siteText1.innerText = ""; //1つ目の広告の説明
-siteImg1.src = "";　　　　//1つ目の広告の画像のリンク
-siteA1.href = "";　　　　 //1つ目の広告のサイトのリンク
-siteA1.title = "";　　　　//1つ目の広告のtitle
+siteName1.innerText = "html入門"; //1つ目の広告の見出し
+siteText1.innerText = "どうもこんにちは。今回はhtml入門というのをやっ..."; //1つ目の広告の説明
+siteImg1.src = "html.png";　　　　//1つ目の広告の画像のリンク
+siteA1.href = "html入門.html";　　　　 //1つ目の広告のサイトのリンク
+siteA1.title = "html入門";　　　　//1つ目の広告のtitle
 
-siteName2.innerText = ""; //2つ目の広告の見出し
-siteText2.innerText = ""; //2つ目の広告の説明
-siteImg2.src = "";　　　　//2つ目の広告の画像のリンク
-siteA2.href = "";　　　　 //2つ目の広告のサイトのリンク
-siteA2.title = "";　　　　//2つ目の広告のtitle
+siteName2.innerText = "MathJaxについて"; //2つ目の広告の見出し
+siteText2.innerText = "どうもこんにちは。今回は~~のようにhtmlで数式を..."; //2つ目の広告の説明
+siteImg2.src = "プログラミング.png";　　　　//2つ目の広告の画像のリンク
+siteA2.href = "MathJaxについて.html";　　　　 //2つ目の広告のサイトのリンク
+siteA2.title = "MathJaxについて";　　　　//2つ目の広告のtitle
