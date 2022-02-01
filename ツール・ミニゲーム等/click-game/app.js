0b6ab42156b4a0f826cfdffaf396b6d1c4495d0c
yamath <95947812+tiger-math@users.noreply.github.com>
2022-02-01 14:23:26 +0900

    Update app.js

--- ツール・ミニゲーム等/click-game/app.js
+++ ツール・ミニゲーム等/click-game/app.js
@@ -7,8 +7,14 @@ var next_goal = 10;
 const $text_click = document.getElementById('text_click');
 const $button_click = document.getElementById('button_click');
 const $btn = document.getElementsByClassName('btn');
+var btn0_num = 0;
 var btn1_num = 0;
 var btn2_num = 0;
+var btn3_num = 0;
+var btn0_point = 1000;
+var btn1_point = 1000;
+var btn2_point = 1500;
+var btn3_point = 10000;
 
 //関数定義
 const write_point = (new_point) => {
@@ -45,30 +51,40 @@ function click_button() {
 
 //関数ーボタン定義
 const btn0 = () => {
-if(point >= 1000){
-  write_point(point-1000);
+if(point >= btn0_point){
+  write_point(point-btn0_point);
   acceleration++;
+  btn0_num++;
+  btn0_point = btn0_point + 100;
+  $btn[0].innerText = '加速度を上げる(-'+btn0_point+'point)';
  } else {alert('ポイントが足りません！')}
 }
 
 const btn1 = () => {
-if(point >= 1000){
-  write_point(point-1000);
+if(point >= btn1_point){
+  write_point(point-btn1_point);
   btn1_num++;
+  btn1_point = btn1_point + 100;
+  $btn[1].innerText = '1秒に1回クリック(-'+btn1_point+'point)';
  } else {alert('ポイントが足りません！')}
 }
 
 const btn2 = () => {
-if(point >= 1500){
-  write_point(point-1500);
+if(point >= btn2_point){
+  write_point(point-btn2_point);
   btn2_num++;
+  btn2_point = btn2_point + 100;
+  $btn[2].innerText = '1秒に2回クリック(-'+btn2_point+'point)';
  } else {alert('ポイントが足りません！')}
 }
 
 const btn3 = () => {
-if(point >= 100000){
-  write_point(point-100000);
+if(point >= btn3_point){
+  write_point(point-btn3_point);
   acceleration = acceleration * 2;
+  btn3_num++;
+  btn3_point = btn3_point * 2;
+  $btn[3].innerText = '加速度を2倍にする(-'+btn3_point+'point)';
  } else {alert('ポイントが足りません！')}
 }
 
