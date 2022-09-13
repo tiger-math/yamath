2cda96ba2380e7f62723b14ebe0b76ae0bd32033
yamath <95947812+tiger-math@users.noreply.github.com>
2022-09-14 07:27:02 +0900

    Add files via upload

---
+++ markdown/app.js
@@ -0,0 +1,26 @@
+let last_input, input;
+// ---------------preview---------------
+function preview() {
+    $('#preview').innerHTML = '';
+    input = $('#input').innerText.split('\n');
+    console.log(input);
+    //h(n), br
+    for (i=0; i<input.length; i++) {
+        for (i2=0, i2<6, i2++) {
+            h(n+1, input[i]);
+        }
+    }
+}
+
+// ---------------h(n)---------------
+function h(n, content) {
+
+}
+
+// ---------------setInterval---------------
+setInterval(function() {
+    if ($('#input').innerText != last_input) {
+        last_input = $('#input').innerText;
+        preview();
+    }
+},16)
